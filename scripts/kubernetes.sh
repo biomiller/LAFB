f133f805f9d5e975507d2605cea90198289be3cb
biomiller <35339306+biomiller@users.noreply.github.com>
2019-07-19 10:29:33 +0100

    Update kubernetes.sh

--- scripts/kubernetes.sh
+++ scripts/kubernetes.sh
@@ -11,19 +11,19 @@ cd ~/LAFB
 
 kubectl apply -f ./mongo/
 
-kubectl apply -f /db-connector/
+kubectl apply -f /db_connector/
 
-kubectl apply -f /prize-generator/
+kubectl apply -f /prize_generator/
 
-kubectl apply -f /number-generator/
+kubectl apply -f /number_generator/
 
 kubectl apply -f ./server/
 
-kubectl apply -f ./notification-server/
+kubectl apply -f ./notification_server/
 
-kubectl apply -f ./text-generator/
+kubectl apply -f ./text_generator/
 
-kubectl apply -f ./static-website/
+kubectl apply -f ./static_website/
 
 kubectl apply -f ./jenkins/
 
